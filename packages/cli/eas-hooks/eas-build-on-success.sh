de782bdc9ecb7de571c892581cf60ed29c099d2d
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2024-03-26 11:59:19 +0100

    extract eas on success hook

--- packages/cli/eas-hooks/eas-build-on-success.sh
+++ packages/cli/eas-hooks/eas-build-on-success.sh
@@ -13,26 +13,52 @@ if [[ "$EAS_BUILD_RUNNER" != "eas-build" ]]; then
     exit
 fi
 
-if [[ "$EAS_BUILD_PROFILE" == "preview" ]]; then
+SHERLO_BUILD_PROFILE=$1
+if [ -z "$SHERLO_BUILD_PROFILE" ]; then
+    echo "Error: You must provide the EAS profile as an argument to this script."
+    exit 1
+fi
+
+if [[ "$EAS_BUILD_PROFILE" == "$SHERLO_BUILD_PROFILE" ]]; then
+
+    # Install dependencies
+    if [[ "$EAS_BUILD_PLATFORM" == "android" ]]; then
+        sudo apt-get install jq -y
+    elif [[ "$EAS_BUILD_PLATFORM" == "ios" ]]; then
+        brew update
+        brew install jq
+    else
+        # Sherlo is not supported on this platform
+        echo "The platform '$EAS_BUILD_PLATFORM' is not supported by this script."
+        exit 0
+    fi
+
+    # Check for sherlo.json file
+    SHERLO_BUILD_FILE="./.expo/sherlo.json"
+    if [ ! -f "$SHERLO_BUILD_FILE" ]; then
+        echo "Error: File $SHERLO_BUILD_FILE does not exist. If you're using monorepo make sure to provide a valid 'projectRoot' option in CLI or Github Action"
+        exit 1
+    fi
+
+    # Extract buildIndex from sherlo.json
+    buildIndex=$(jq '.buildIndex' "$SHERLO_BUILD_FILE")
+    if [ "$buildIndex" = "null" ] || ! [[ "$buildIndex" =~ ^[0-9]+$ ]]; then
+        echo "Error: 'buildIndex' is either not present or not a valid number in $SHERLO_BUILD_FILE."
+        exit 1
+    fi
+
 
     if [[ "$EAS_BUILD_PLATFORM" == "android" ]]; then
-        # Install jq to parse JSON
-        sudo apt-get install jq -y
-
         # Details on Android build paths: https://docs.expo.dev/build-reference/android-builds/
         android_path=$(jq -r '.builds.android."'"$EAS_BUILD_PROFILE"'"."applicationArchivePath"' eas.json)
         if [[ -z "$android_path" || "$android_path" == "null" ]]; then
             android_path="android/app/build/outputs/apk/release/app-release.apk"
         fi
 
-        yarn sherlo --asyncUploadCommitHash=$EAS_BUILD_GIT_COMMIT_HASH --token=$SHERLO_TOKEN --android=$android_path 
+        yarn sherlo --asyncUploadCommitHash=$buildIndex --token=$SHERLO_TOKEN --android=$android_path 
     fi
 
     if [[ "$EAS_BUILD_PLATFORM" == "ios" ]]; then
-        # Install jq to parse JSON
-        brew update
-        brew install jq
-
         # Details on iOS build paths: https://docs.expo.dev/build-reference/ios-builds/
         ios_path=$(jq -r '.builds.ios."'"$EAS_BUILD_PROFILE"'"."applicationArchivePath"' eas.json)
         if [[ -z "$ios_path" || "$ios_path" == "null" ]]; then
