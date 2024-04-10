6bc758f704da165a02da4d1132bc9bf64ab8d293
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2024-04-10 23:13:43 +0200

    fix: use verified devices and os versions

--- examples/expo-example/.storybook/preview.ts
+++ examples/expo-example/.storybook/preview.ts
@@ -4,6 +4,7 @@ import { withBackgrounds } from '@storybook/addon-ondevice-backgrounds';
 const preview: Preview = {
   decorators: [withBackgrounds],
   parameters: {
+    noSafeArea: true,
     backgrounds: {
       default: 'default',
       values: [
