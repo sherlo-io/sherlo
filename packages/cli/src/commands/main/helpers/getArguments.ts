3d7c794f4c538e63c04780dff5ecf34e6954bee1
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2024-04-23 19:37:55 +0200

    use commander instead of yargs

--- packages/cli/src/commands/main/helpers/getArguments.ts
+++ packages/cli/src/commands/main/helpers/getArguments.ts
@@ -1,8 +1,7 @@
 import { Build, Platform } from '@sherlo/api-types';
 import { defaultDeviceOsLanguage, defaultDeviceOsTheme } from '@sherlo/shared';
 import nodePath from 'path';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
+import { Command } from 'commander';
 import { getErrorMessage } from '../utils';
 import { Config, InvalidatedConfig, Mode } from '../types';
 import {
@@ -54,7 +53,7 @@ type AsyncUploadArguments = {
 };
 
 function getArguments(githubActionParameters?: Parameters): Arguments {
-  const parameters = githubActionParameters ?? cliParameters;
+  const parameters = githubActionParameters ?? program.parse(process.argv).opts();
   const updatedParameters = updateParameters(parameters);
 
   const config = parseConfigFile(updatedParameters.config);
@@ -125,39 +124,23 @@ function getArguments(githubActionParameters?: Parameters): Arguments {
 const DEFAULT_CONFIG_PATH = 'sherlo.config.json';
 const DEFAULT_PROJECT_ROOT = '.';
 
-const cliParameters = yargs(hideBin(process.argv))
-  .option('config', {
-    default: DEFAULT_CONFIG_PATH,
-    description: 'Path to Sherlo config',
-    type: 'string',
-  })
-  .option('async', {
-    description: 'Run Sherlo in async mode, meaning you don’t have to provide builds immediately',
-    type: 'boolean',
-  })
-  .option('asyncBuildIndex', {
-    description:
-      'If you want to upload android or ios build to existing sherlo build in async mode, you need to provide index of build you want to update',
-    type: 'number',
-  })
-  .option('projectRoot', {
-    default: DEFAULT_PROJECT_ROOT,
-    description:
-      'Use this option to specify the root of the react native project when working with monorepo',
-    type: 'string',
-  })
-  .option('token', {
-    description: 'Sherlo project token',
-    type: 'string',
-  })
-  .option('android', {
-    description: 'Path to Android build in .apk format',
-    type: 'string',
-  })
-  .option('ios', {
-    description: 'Path to iOS simulator build in .app or .tar.gz file format',
-    type: 'string',
-  }).argv as Parameters;
+const program = new Command();
+
+program
+  .option('--config <path>', 'Path to Sherlo config', DEFAULT_CONFIG_PATH)
+  .option(
+    '--async',
+    'Run Sherlo in async mode, meaning you don’t have to provide builds immediately'
+  )
+  .option('--asyncBuildIndex <number>', 'Index of build you want to update in async mode', parseInt)
+  .option(
+    '--projectRoot <path>',
+    'Root of the react native project when working with monorepo',
+    DEFAULT_PROJECT_ROOT
+  )
+  .option('--token <token>', 'Sherlo project token')
+  .option('--android <path>', 'Path to Android build in .apk format')
+  .option('--ios <path>', 'Path to iOS simulator build in .app or .tar.gz file format');
 
 function updateParameters(parameters: Parameters): Parameters<'withDefaults'> {
   // Set defaults if are not defined (can happen in GitHub action case)
