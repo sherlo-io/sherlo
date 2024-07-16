85d246e07fe6fc98e0f518a2ab655098cf1ba0b3
Dawid Krajewski <dawid@devine.team>
2024-07-16 14:50:30 +0200

    up

--- packages/react-native-storybook/src/getStorybook/hooks/useStoryEmitter.ts
+++ packages/react-native-storybook/src/getStorybook/hooks/useStoryEmitter.ts
@@ -4,21 +4,20 @@ import { useEffect } from 'react';
 
 function useStoryEmitter(updateRenderedStoryId: (storyId: string) => void): (id: string) => void {
   useEffect(() => {
-    function handleStoryRendered(...args: any[]): void {
+    const handleStoryRendered = (...args: any[]): void => {
       const storyId = args?.[0]?.storyId;
       updateRenderedStoryId(storyId);
-    }
+    };
 
-    function initialize(): Promise<void> {
-      return addons.ready().then((_channel) => {
+    const initialize = (): Promise<void> =>
+      addons.ready().then((_channel) => {
         _channel.on(SET_CURRENT_STORY, handleStoryRendered);
       });
-    }
 
     setTimeout(() => initialize(), 500);
-  }, [updateRenderedStoryId]);
+  }, []);
 
-  return function (storyId: string) {
+  return (storyId: string) => {
     addons.ready().then((_channel) => {
       _channel.emit(SET_CURRENT_STORY, { storyId });
       // if we don't call it twice going back from preview to original mode doesn't work
