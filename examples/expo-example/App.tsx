ffb85828f41889c2d2b8d6d96c09423c022e9b15
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2024-08-08 17:30:22 +0200

    Update App.tsx

--- examples/expo-example/App.tsx
+++ examples/expo-example/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback } from 'react';
 import { View, Text, StyleSheet, Button, StatusBar as RNStatusBar } from 'react-native';
 import {
   useFonts,
@@ -14,22 +14,16 @@ import { StatusBar } from 'expo-status-bar';
 // @ts-ignore
 const isStandaloneStorybookBuild = process.env.EXPO_PUBLIC_STORYBOOK_ONLY === 'true';
 // @ts-ignore
-const isDevelopmentBuild = process.env.PROD_BUILD === 'false';
+const isDevelopmentBuild = process.env.PROD_BUILD !== 'true';
 
-console.log('isStandaloneStorybookBuild', isStandaloneStorybookBuild);
-console.log('isDevelopmentBuild', isDevelopmentBuild);
+if (!isStandaloneStorybookBuild && isDevelopmentBuild) {
+  const registerStorybook = require('@sherlo/react-native-storybook').registerStorybook;
+  const Storybook = require('./.storybook').default;
 
-// if (!isStandaloneStorybookBuild && isDevelopmentBuild) {
-const registerStorybook = require('@sherlo/react-native-storybook').registerStorybook;
-const Storybook = require('./.storybook').default;
-
-registerStorybook(() => <Storybook />);
-// }
+  registerStorybook(() => <Storybook />);
+}
 
 SplashScreen.preventAutoHideAsync();
-
-// global.SHERLO_VERIFY_SETUP = true;
-// global.SHERLO_TEST_CONFIG = {};
 
 export default function App() {
   let [fontsLoaded, fontError] = useFonts({
@@ -44,24 +38,6 @@ export default function App() {
       await SplashScreen.hideAsync();
     }
   }, [fontsLoaded, fontError]);
-
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((c) => c + 1);
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
-
-  console.log('counter', counter);
 
   // eslint-disable-next-line react/no-unstable-nested-components
   const App = () => {
@@ -78,7 +54,6 @@ export default function App() {
         <RNStatusBar backgroundColor="red" />
         <Text style={{ textAlign: 'center' }}>
           {'Open Dev Menu and select "Toggle Storybook" \nor click the button below'}
-          {counter}
         </Text>
         <Button title="Open Storybook" onPress={openStorybook} />
       </View>
