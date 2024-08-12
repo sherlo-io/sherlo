2e42b957d4c24568ae70fceebcd773cb9c231a02
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2024-08-13 00:34:49 +0200

    Update index.ts

--- packages/react-native-storybook/src/index.ts
+++ packages/react-native-storybook/src/index.ts
@@ -1,5 +1,5 @@
 export { default as getStorybook } from './getStorybook';
 export { default as openStorybook } from './openStorybook';
-export { default as registerStorybook } from './registerStorybook';
+export { default as registerStorybook, loaded } from './registerStorybook';
 
 // export { default as getSherloParameters } from './getSherloParameters';
