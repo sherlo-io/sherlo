a9a40a731b0b73dda98f9d7c3a4342ba43832a12
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2024-11-06 14:34:31 +0100

    fix figma comparison story

--- examples/local-builds/src/components/WithFigmaComparison/WithFigmaComparison.tsx
+++ examples/local-builds/src/components/WithFigmaComparison/WithFigmaComparison.tsx
@@ -4,7 +4,14 @@ import { View, Text, StyleSheet } from 'react-native';
 const PlatformSpecificComponent = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This component will be compared to the Figma design</Text>
+      <View style={styles.purpleBar}>
+        <View style={styles.greenBox} />
+      </View>
+      <View style={styles.greyContainer}>
+        <View style={styles.redRectangle}>
+          <Text style={styles.text}>This component will be compared to the Figma design</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -14,10 +21,42 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+  },
+  purpleBar: {
+    width: 'auto',
+    marginHorizontal: 36,
+    height: 20,
+    backgroundColor: '#800080',
+    marginBottom: 48,
+    alignSelf: 'stretch',
+  },
+  greyContainer: {
+    width: 282,
+    height: 282,
+    backgroundColor: '#D3D3D3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  redRectangle: {
+    width: 230,
+    height: 230,
+    backgroundColor: '#FF0000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  greenBox: {
+    position: 'absolute',
+    width: 52,
+    height: 52,
+    backgroundColor: '#008000',
+    bottom: 10,
+    left: 20,
   },
   text: {
-    fontSize: 18,
-    marginBottom: 20,
+    fontSize: 17,
+    color: '#FFFFFF',
+    textAlign: 'center',
   },
 });
 
