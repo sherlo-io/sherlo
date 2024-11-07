98c76b32911909d86e8004e3ffd4e79817a6f10b
Dawid Krajewski <dawid@devine.team>
2024-11-07 17:54:40 +0100

    up

--- packages/cli/src/helpers/printHeader.ts
+++ packages/cli/src/helpers/printHeader.ts
@@ -1,9 +1,10 @@
+import chalk from 'chalk';
 import gradientString from 'gradient-string';
 
 const color = {
+  reported: 'FFB36C',
   approved: '79E8A5',
   noChanges: '64B5F6',
-  unreviewed: 'FF906C',
 };
 
 const header = `
@@ -14,13 +15,15 @@ const header = `
     88K      888 "88b d8P  Y8b 888"    888 d88""88b 
     "Y8888b. 888  888 88888888 888     888 888  888 
          X88 888  888 Y8b.     888     888 Y88..88P 
-     88888P' 888  888  "Y8888  888     888  "Y88P"
-
-Make sure your mobile app looks perfect on every device
+    '88888P' 888  888  "Y8888  888     888  "Y88P"
 `;
 
 function printHeader(): void {
-  console.log(gradientString(color.unreviewed, color.approved, color.noChanges)(header));
+  console.log(gradientString(color.reported, color.approved, color.noChanges)(header));
+
+  console.log(chalk.dim(chalk.italic('Make sure your mobile app looks perfect on every device')));
+
+  console.log();
 }
 
 export default printHeader;
