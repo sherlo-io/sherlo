8cc2d921de4f356ba356ddd727154b72f8e906db
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2024-11-08 15:48:19 +0100

    Update Layout.tsx

--- packages/react-native-storybook/src/getStorybook/components/Layout.tsx
+++ packages/react-native-storybook/src/getStorybook/components/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, ViewProps, Platform, useColorScheme } from 'react-native';
+import { View, StyleSheet, ViewProps, Platform } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface LayoutProps extends ViewProps {
@@ -8,7 +8,6 @@ interface LayoutProps extends ViewProps {
 
 export const Layout: React.FC<LayoutProps> = ({ shouldAddSafeArea, children, ...props }) => {
   const insets = useSafeAreaInsets();
-  const theme = useColorScheme();
 
   const showSafeArea = shouldAddSafeArea && Platform.OS === 'ios';
 
@@ -18,7 +17,6 @@ export const Layout: React.FC<LayoutProps> = ({ shouldAddSafeArea, children, ...
       style={[
         StyleSheet.absoluteFillObject,
         showSafeArea && { paddingTop: insets.top, paddingBottom: insets.bottom },
-        { backgroundColor: theme === 'dark' ? 'black' : 'white' },
       ]}
       {...props}
     >
