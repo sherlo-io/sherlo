6ee6483995a794869666d38554c36a715740cf36
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2024-11-08 15:13:22 +0100

    fix bg in safe area

--- packages/react-native-storybook/src/getStorybook/components/Layout.tsx
+++ packages/react-native-storybook/src/getStorybook/components/Layout.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { View, StyleSheet, ViewProps, Platform } from 'react-native';
+import { View, StyleSheet, ViewProps, Platform, useColorScheme } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface LayoutProps extends ViewProps {
   shouldAddSafeArea: boolean;
 }
 
-export const Layout: React.FC<LayoutProps> = ({ shouldAddSafeArea, children, style, ...props }) => {
+export const Layout: React.FC<LayoutProps> = ({ shouldAddSafeArea, children, ...props }) => {
   const insets = useSafeAreaInsets();
+  const theme = useColorScheme();
 
   const showSafeArea = shouldAddSafeArea && Platform.OS === 'ios';
 
@@ -17,7 +18,7 @@ export const Layout: React.FC<LayoutProps> = ({ shouldAddSafeArea, children, sty
       style={[
         StyleSheet.absoluteFillObject,
         showSafeArea && { paddingTop: insets.top, paddingBottom: insets.bottom },
-        style,
+        { backgroundColor: theme === 'dark' ? 'black' : 'white' },
       ]}
       {...props}
     >
