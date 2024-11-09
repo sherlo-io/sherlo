5fcd3a3197d307d333ba04369bbe3aaf378fe4a0
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2024-11-10 00:57:59 +0100

    add many devices testing

---
+++ testing/expo-storybook-8/src/components/ManyStories/ManyStories.stories.tsx
@@ -0,0 +1,177 @@
+import type { Meta } from '@storybook/react';
+import { StyleSheet, View } from 'react-native';
+
+export default {
+  component: View,
+} as Meta<typeof View>;
+
+export const Red = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#FF0000',
+    },
+  },
+};
+
+export const Blue = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#0000FF',
+    },
+  },
+};
+
+export const Green = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#00FF00',
+    },
+  },
+};
+
+export const Yellow = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#FFFF00',
+    },
+  },
+};
+
+export const Purple = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#800080',
+    },
+  },
+};
+
+export const Orange = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#FFA500',
+    },
+  },
+};
+
+export const Pink = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#FFC0CB',
+    },
+  },
+};
+
+export const Brown = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#A52A2A',
+    },
+  },
+};
+
+export const Gray = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#808080',
+    },
+  },
+};
+
+export const Cyan = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#00FFFF',
+    },
+  },
+};
+
+export const Magenta = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#FF00FF',
+    },
+  },
+};
+
+export const Lime = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#32CD32',
+    },
+  },
+};
+
+export const Teal = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#008080',
+    },
+  },
+};
+
+export const Indigo = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#4B0082',
+    },
+  },
+};
+
+export const Violet = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#EE82EE',
+    },
+  },
+};
+
+export const Maroon = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#800000',
+    },
+  },
+};
+
+export const Navy = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#000080',
+    },
+  },
+};
+
+export const Olive = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#808000',
+    },
+  },
+};
+
+export const Turquoise = {
+  args: {
+    style: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: '#40E0D0',
+    },
+  },
+};
