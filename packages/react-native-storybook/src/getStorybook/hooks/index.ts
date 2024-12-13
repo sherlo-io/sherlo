6c3bef71ded05be560154a8e55bedffb13032789
Dawid Krajewski <dawid@devine.team>
2024-12-13 13:55:26 +0100

    fix: hide SplashScreen (#46)

--- packages/react-native-storybook/src/getStorybook/hooks/index.ts
+++ packages/react-native-storybook/src/getStorybook/hooks/index.ts
@@ -1,3 +1,4 @@
+export { default as useHideSplashScreen } from './useHideSplashScreen';
 export { default as useOriginalMode } from './useOriginalMode';
 export { default as useStoryEmitter } from './useStoryEmitter';
 export { default as useTestingMode } from './useTestingMode';
