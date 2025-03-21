aaa45608e73169e010a36172e7377d9e89525d82
Dawid Krajewski <dawid@devine.team>
2025-03-21 12:46:29 +0100

    up

--- packages/cli/src/helpers/getCwd.ts
+++ packages/cli/src/helpers/getCwd.ts
@@ -1,10 +1,5 @@
 function getCwd(): string {
-  /**
-   * Use INIT_CWD to get the actual directory where command was run
-   * Package managers like yarn change process.cwd() to the directory with package.json
-   * but we want to use the directory where user actually ran the command
-   */
-  return process.env.INIT_CWD ?? process.cwd();
+  return process.cwd();
 }
 
 export default getCwd;
