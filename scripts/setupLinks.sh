d1fbc2db92b55501c01e4d97d97144828db30f33
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2025-03-31 23:09:58 +0200

    up

---
+++ scripts/setupLinks.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ROOT_DIR="$(pwd)"
+
+echo "Root directory: $ROOT_DIR"
+
+REACT_NATIVE_STORYBOOK="${ROOT_DIR}/packages/react-native-storybook"
+TESTING_COMPONENTS="${ROOT_DIR}/testing/testing-components"
+EXPO_STORYBOOK="${ROOT_DIR}/testing/expo-storybook-8"
+RN_STORYBOOK="${ROOT_DIR}/testing/rn-storybook-7"
+
+echo "Installing dependencies..."
+cd "$EXPO_STORYBOOK" && yarn
+cd "$RN_STORYBOOK" && yarn
+
+echo "Linking shared packages..."
+cd "$REACT_NATIVE_STORYBOOK" && yarn link
+cd "$TESTING_COMPONENTS" && yarn link
+
+echo "Linking shared packages into expo-storybook-8..."
+cd "$EXPO_STORYBOOK" && \
+yarn link @sherlo/react-native-storybook && \
+yarn link @sherlo/testing-components
+
+echo "Linking shared packages into rn-storybook-7..."
+cd "$RN_STORYBOOK" && \
+yarn link @sherlo/react-native-storybook && \
+yarn link @sherlo/testing-components
