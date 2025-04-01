858ebc24b8ca59f337c33c90d19030b0dca61b6c
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2025-04-01 18:35:20 +0200

    up

---
+++ testing/rn-storybook-7/src/testing-components/NativeCrashStory/NativeCrashStory.stories.tsx
@@ -0,0 +1,8 @@
+import type { Meta } from '@storybook/react';
+import NativeCrashStory from './NativeCrashStory';
+
+export default {
+  component: NativeCrashStory,
+} as Meta<typeof NativeCrashStory>;
+
+export const Basic = {};
