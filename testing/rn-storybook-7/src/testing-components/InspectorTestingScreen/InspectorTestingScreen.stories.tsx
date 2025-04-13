4b17b7b5b3e301fd94b2deddc37027fbbe319cd6
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2025-04-13 11:10:09 +0200

    up

--- testing/rn-storybook-7/src/testing-components/InspectorTestingScreen/InspectorTestingScreen.stories.tsx
+++
@@ -1,8 +0,0 @@
-import type { Meta } from '@storybook/react';
-import { InspectorTestingScreen } from '@sherlo/testing-components';
-
-export default {
-  component: InspectorTestingScreen,
-} as Meta<typeof InspectorTestingScreen>;
-
-export const Basic = {};
