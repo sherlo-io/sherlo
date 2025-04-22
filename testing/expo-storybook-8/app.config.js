8f5044794b7e19fa9e062310da729529a40a067c
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2025-04-22 15:30:54 +0200

    Update app.config.js

--- testing/expo-storybook-8/app.config.js
+++ testing/expo-storybook-8/app.config.js
@@ -1,37 +1,45 @@
-module.exports = () => ({
-  expo: {
-    owner: 'sherlo',
-    name: 'Sherlo Expo Example',
-    slug: 'sherlo-expo-example',
-    version: '1.0.0',
-    orientation: 'portrait',
-    userInterfaceStyle: 'automatic',
-    splash: {
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff',
-    },
-    newArchEnabled: process.env.USE_NEW_ARCH === 'true',
-    updates: {
-      enabled: true,
-      fallbackToCacheTimeout: 300000,
-      url: 'https://u.expo.dev/4cb166d7-f774-49a9-9aa0-e5533347bcd1',
-    },
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: 'com.sherlo.example',
-      usesNonExemptEncryption: false,
-    },
-    android: {
-      package: 'com.sherlo.example',
-    },
-    runtimeVersion: {
-      policy: 'appVersion',
-    },
-    plugins: ['./plugin.js', 'expo-localization'],
-    extra: {
-      eas: {
-        projectId: '4cb166d7-f774-49a9-9aa0-e5533347bcd1',
+module.exports = () => {
+  const plugins = ['expo-localization'];
+
+  if (process.env.USE_NEW_ARCH === 'true') {
+    plugins.push('./plugin.js');
+  }
+
+  return {
+    expo: {
+      owner: 'sherlo',
+      name: 'Sherlo Expo Example',
+      slug: 'sherlo-expo-example',
+      version: '1.0.0',
+      orientation: 'portrait',
+      userInterfaceStyle: 'automatic',
+      splash: {
+        resizeMode: 'contain',
+        backgroundColor: '#ffffff',
+      },
+      newArchEnabled: process.env.USE_NEW_ARCH === 'true',
+      updates: {
+        enabled: true,
+        fallbackToCacheTimeout: 300000,
+        url: 'https://u.expo.dev/4cb166d7-f774-49a9-9aa0-e5533347bcd1',
+      },
+      ios: {
+        supportsTablet: true,
+        bundleIdentifier: 'com.sherlo.example',
+        usesNonExemptEncryption: false,
+      },
+      android: {
+        package: 'com.sherlo.example',
+      },
+      runtimeVersion: {
+        policy: 'appVersion',
+      },
+      plugins,
+      extra: {
+        eas: {
+          projectId: '4cb166d7-f774-49a9-9aa0-e5533347bcd1',
+        },
       },
     },
-  },
-});
+  };
+};
