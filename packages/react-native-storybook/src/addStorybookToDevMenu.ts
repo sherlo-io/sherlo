ebb7d0e6967358a5c5bc4db306ef79636a0ca8b9
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2025-04-24 00:40:48 +0200

    Update addStorybookToDevMenu.ts

--- packages/react-native-storybook/src/addStorybookToDevMenu.ts
+++ packages/react-native-storybook/src/addStorybookToDevMenu.ts
@@ -17,8 +17,19 @@ function addStorybookToDevMenu() {
   DevSettings.addMenuItem(MENU_LABEL, toggleStorybook);
 
   if (ExpoDevMenu?.registerDevMenuItems) {
-    // TODO: Problem with new arch, when this gets called for second time after user
-    // toggles storybook, it doesn't work. The function is not called at all.
+    // TODO: Expo Bug present only on New Arch
+    //
+    // 1. Open the dev menu
+    // 2. Select custom dev menu item
+    // Result: it behaves as expected
+    // 3. Choose "reload"
+    // 4. Again select custom dev menu item
+    // Result: it doesn't respond
+    //
+    // The same thing happens if we choose "Toggle Storybook" from the dev menu because
+    // it reloads the app as a part of it's functionality. Because of that, once
+    // user enters Storybook mode, they cannot leave by selecting "Toggle Storybook"
+    // from the dev menu again.
     ExpoDevMenu.registerDevMenuItems([{ name: MENU_LABEL, callback: toggleStorybook }]);
   }
 
