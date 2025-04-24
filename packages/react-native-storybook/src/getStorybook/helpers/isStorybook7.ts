75f25c5fa45b8dd6a7d41217904dbb2e8d584770
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2025-04-24 22:59:24 +0200

    fix(react-native-storybook): improve SB version detection to not throw warnings

--- packages/react-native-storybook/src/getStorybook/helpers/isStorybook7.ts
+++ packages/react-native-storybook/src/getStorybook/helpers/isStorybook7.ts
@@ -1,8 +1,15 @@
-let isStorybook7 = true;
+// Detect Storybook 7 by checking for version-specific exports
+let isStorybook7 = false;
+
 try {
-  // This file is present only in Storybook 7
-  require('@storybook/react-native/V6');
+  // Try to import the main Storybook package
+  const storybook = require('@storybook/react-native');
+
+  // updateView is exported in v8 but not in v7
+  // If it doesn't exist, we're likely on v7
+  isStorybook7 = !('updateView' in storybook);
 } catch (error) {
+  // Default to false if we can't detect
   isStorybook7 = false;
 }
 
