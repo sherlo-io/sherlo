a45d069024a7f7e8e5fbb564e71ef4ed5fe1df36
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2025-05-13 21:33:18 +0200

    Update TamaguiImage.tsx

--- testing/expo-storybook-8/src/testing-components/TamaguiImage/TamaguiImage.tsx
+++ testing/expo-storybook-8/src/testing-components/TamaguiImage/TamaguiImage.tsx
@@ -1,14 +1,19 @@
+import React from 'react';
+import { Text } from 'react-native';
 import { Image } from 'tamagui';
 
 function TamaguiImage({ uri }: TamaguiImageProps) {
   return (
-    <Image
-      source={{
-        uri,
-        width: 200,
-        height: 300,
-      }}
-    />
+    <>
+      <Image
+        source={{
+          uri,
+          width: 200,
+          height: 300,
+        }}
+      />
+      <Text>URI: {uri}</Text>
+    </>
   );
 }
 
