c95d5cf466822bbaaf041061c49984e6ce270a95
Michał Ziółkowski <ziolkowskimichal93@gmail.com>
2025-07-06 15:39:25 +0200

    fix(react-native-storybook): android toggle storybook refactor

---
+++ packages/react-native-storybook/android/src/main/java/io/ReactInstanceHolder.java
@@ -0,0 +1,7 @@
+package io.sherlo.storybookreactnative;
+
+import com.facebook.react.ReactInstanceManager;
+
+public interface ReactInstanceHolder {
+    ReactInstanceManager getReactInstanceManager();
+}
