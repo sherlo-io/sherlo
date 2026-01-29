3fb23086f1ba33c29ed29bdaf042f20ce6627107
Dawid Krajewski <dawid@devine.team>
2026-01-29 21:14:24 +0100

    no message

--- examples/standard/.rnstorybook/index.ts
+++ examples/standard/.rnstorybook/index.ts
@@ -1,7 +1,10 @@
-import { getStorybook } from '@sherlo/react-native-storybook';
+// ⚠️ Modified for Sherlo - see https://sherlo.io/docs/setup#storybook-component
+
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getStorybook } from '@sherlo/react-native-storybook';
 import { view } from './storybook.requires';
 
+// Use getStorybook() instead of view.getStorybookUI()
 const StorybookUIRoot = getStorybook(view, {
   storage: {
     getItem: AsyncStorage.getItem,
