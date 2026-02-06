d45f9723ca7e1a3ff04dd80b415ed833ca35f2fe
Dawid Krajewski <dawid@devine.team>
2026-02-06 17:34:05 +0100

    no message

--- examples/OLD/_eas-cloud-build/metro.config.js
+++
@@ -1,6 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withStorybook } = require('@storybook/react-native/metro/withStorybook');
-
-const config = getDefaultConfig(__dirname);
-
-module.exports = withStorybook(config);
